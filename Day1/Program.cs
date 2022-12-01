e60f7444ded8b05b7e4b0f91ac298368b8da8f4d
Ovvugt <oscar.vugt@gmail.com>
2022-12-01 16:35:32 +0100

    Advent of Code Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var calories = File.ReadAllText("elf_calories.txt").Split("\r\n\r\n");
+
+var elves = calories.Select<string, int>(elf => elf.Split('\n').Select(int.Parse).ToList().Sum()).ToList();
+Console.WriteLine(elves.Max()); // Solution Part One
+
+elves.Sort();
+Console.WriteLine(elves.TakeLast(3).Sum()); // Solution Part Two
