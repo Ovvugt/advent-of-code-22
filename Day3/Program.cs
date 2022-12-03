2164491c01d5ef18a9678d270da48173cc9c5019
Oscar van Vugt <o.vugt@bpiservices.eu>
2022-12-03 17:24:08 +0100

    Advent of Code Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,35 @@
+﻿var rucksacks = File.ReadAllText("rucksacks.txt").Split('\n');
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+//Solution part 1
+var score = 0;
+
+foreach (var rucksack in rucksacks)
+{
+    var firstHalf = rucksack[..(rucksack.Length/2)];
+    var secondHalf = rucksack[(rucksack.Length / 2)..];
+    foreach (var character in firstHalf.Where(character => secondHalf.Contains(character)))
+    {
+        score += alphabet.IndexOf(character.ToString().ToLower(), StringComparison.Ordinal) + 1 + (char.IsUpper(character) ? 26 : 0);
+        break;
+    }
+}
+
+Console.WriteLine(score);
+
+//Solution part 2
+score = 0;
+
+for (var i = 0; i < rucksacks.Length; i += 3)
+{
+    var elf1 = rucksacks[i];
+    var elf2 = rucksacks[i + 1];
+    var elf3 = rucksacks[i + 2];
+    foreach (var character in elf1.Where(character => elf2.Contains(character) && elf3.Contains(character)))
+    {
+        score += alphabet.IndexOf(character.ToString().ToLower(), StringComparison.Ordinal) + 1 + (char.IsUpper(character) ? 26 : 0);
+        break;
+    }
+}
+
+Console.WriteLine(score);
