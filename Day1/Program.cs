a41ed31b1f1324d28500070efe68768cb395a79f
Ovvugt <oscar.vugt@gmail.com>
2022-12-04 19:50:57 +0100

    Advent of Code Day 4

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,6 +1,6 @@
 ﻿var calories = File.ReadAllText("elf_calories.txt").Split("\r\n\r\n");
 
-var elves = calories.Select(elf => elf.Split('\n').Select(int.Parse).ToList().Sum()).ToList();
+var elves = calories.Select(elf => elf.Split('\n').Select(int.Parse).Sum()).ToList();
 Console.WriteLine(elves.Max()); // Solution Part One
 
 elves.Sort();
---
+++ Day4/Program.cs
@@ -0,0 +1,31 @@
+﻿var ids = File.ReadAllText("ids.txt").Split('\n');
+
+var containingIds = 0;
+var overlappingIds = 0;
+
+foreach (var id in ids)
+{
+    var ranges = id.Split(',');
+    var rangeOne = ranges[0].Split('-').Select(int.Parse).ToArray();
+    var rangeTwo = ranges[1].Split('-').Select(int.Parse).ToArray();
+
+    if (rangeOne[0] <= rangeTwo[0] && rangeTwo[0] <= rangeOne[1] ||
+        rangeOne[0] <= rangeTwo[1] && rangeTwo[1] <= rangeOne[1] ||
+        rangeTwo[0] <= rangeOne[0] && rangeOne[0] <= rangeTwo[1] ||
+        rangeTwo[0] <= rangeOne[1] && rangeOne[1] <= rangeTwo[1])
+    {
+        overlappingIds++;
+    }
+    else continue;
+
+    if (rangeOne[0] >= rangeTwo[0] && rangeOne[1] <= rangeTwo[1] ||
+    rangeTwo[0] >= rangeOne[0] && rangeTwo[1] <= rangeOne[1])
+    {
+        containingIds++;
+    }
+}
+
+Console.WriteLine(containingIds);
+Console.WriteLine(overlappingIds);
+
+
